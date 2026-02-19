c10049f12e720a06a9ceba59c9802fb334eabffd
Darlei Randel <darley_rom@hotmail.com>
2026-02-19 09:27:11 -0300

    Add Public key

--- iac/kubeadm-cluster/outputs.tf
+++ iac/kubeadm-cluster/outputs.tf
@@ -8,9 +8,3 @@ output "Alb_dns_name" {
     description = "Application Load Balancer DNS Name"
     value = aws_lb.k8s_alb.dns_name
 }
-
-output "private_key_pem" {
-  value     = tls_private_key.k8s_workers.private_key_pem
-  sensitive = true
-}
-
